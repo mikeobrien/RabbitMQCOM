b3f3c4b0d0dcddb22336b09583ff7078f208e0eb
mob <mob@mikeobrien.net>
2014-05-13 12:09:02 -0500

    Basic publishing functionality.

--- src/RabbitMQ/Connection.cs
+++ src/RabbitMQ/Connection.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using System.Net.Sockets;
+using System.Web.;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Web.Script.Serialization;
+using RabbitMQ.Client;
 
 namespace RabbitMQ
 {
@@ -12,9 +14,56 @@ namespace RabbitMQ
     [ClassInterface(ClassInterfaceType.AutoDual)]
     public class Connection
     {
-        public void Publish(string message)
+        private IConnection _connection;
+        private IModel _channel;
+        private JavaScriptSerializer _serializer;
+
+        // amqp://user:pass@hostName:port/vhost
+
+        public void Connect(string uri)
         {
-            File.WriteAllText("c:\\temp\\result.txt", message);
+            if (_connection != null) Disconnect();
+            _connection = new ConnectionFactory { Uri = uri }.CreateConnection();
+            _channel = _connection.CreateModel();
+            _serializer = new JavaScriptSerializer();
+        }
+
+        public void CreateDirectQueue(string exchange, string routingKey, 
+            string queue, bool durable, bool exclusive, bool autoDelete)
+        {
+            CreateQueue(exchange, ExchangeType.Direct, routingKey, queue, durable, exclusive, autoDelete);
+        }
+
+        public void CreateFanoutQueue(string exchange, string routingKey, 
+            string queue, bool durable, bool exclusive, bool autoDelete)
+        {
+            CreateQueue(exchange, ExchangeType.Fanout, routingKey, queue, durable, exclusive, autoDelete);
+        }
+
+        public void CreateTopicQueue(string exchange, string routingKey, 
+            string queue, bool durable, bool exclusive, bool autoDelete)
+        {
+            CreateQueue(exchange, ExchangeType.Topic, routingKey, queue, durable, exclusive, autoDelete);
+        }
+
+        private void CreateQueue(string exchange, string type, string routingKey, 
+            string queue, bool durable, bool exclusive, bool autoDelete)
+        {
+            _channel.ExchangeDeclare(exchange, type);
+            _channel.QueueDeclare(queue, durable, exclusive, autoDelete, null);
+            _channel.QueueBind(queue, exchange, routingKey);
+        }
+
+        public void Publish(string exchange, string routingKey, object @object)
+        {
+            _channel.BasicPublish(exchange, routingKey, null, 
+                Encoding.UTF8.GetBytes(_serializer.Serialize(@object)));
+        }
+
+        public void Disconnect()
+        {
+            _channel.Dispose();
+            _connection.Dispose();
         }
     }
 }
